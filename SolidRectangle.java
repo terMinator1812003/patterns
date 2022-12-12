47d7fff89418987c13610cb1901f7086ed93d297
HARSH NAYAK <100467506+terMinator1812003@users.noreply.github.com>
2022-12-12 11:56:39 +0530

    Add files via upload

---
+++ SolidRectangle.java
@@ -0,0 +1,24 @@
+package patternss;
+
+import java.util.*;
+
+public class SolidRectangle {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+
+        }
+        // System.out.println();
+
+    }
+    
+}
