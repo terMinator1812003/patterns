d6632b604ad5e602c31a14ff821713398403c849
HARSH NAYAK <100467506+terMinator1812003@users.noreply.github.com>
2022-12-13 11:58:19 +0530

    Add files via upload

---
+++ InvertedHalfPyrWithNUmbers.java
@@ -0,0 +1,18 @@
+package patternss;
+
+import java.util.Scanner;
+
+public class InvertedHalfPyrWithNUmbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of n:");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
