754d1c39c2cea73d9f4dc46c37a9e3200fdd627e
HARSH NAYAK <100467506+terMinator1812003@users.noreply.github.com>
2022-12-14 22:46:37 +0530

    Add files via upload

---
+++ Palindromic.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Palindromic {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+}
